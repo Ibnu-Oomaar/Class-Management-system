2d66f80693becb749e77c36863339a961bb6b9ee
Ibnu-Oomaar <apdirsaqmoktar@email.com>
2026-09-01 04:24:30 +0300

    updated

--- frontend/src/helpers/api.ts
+++ frontend/src/helpers/api.ts
@@ -6,7 +6,7 @@ import type { InternalAxiosRequestConfig } from "axios";
 // ============================================================
 
 const API_URL =
-  import.meta.env.VITE_API_URL ?? "http://localhost:5030/api";
+  import.meta.env.VITE_API_URL ?? "http://localhost:5000/api";
 
 // ============================================================
 // AXIOS INSTANCE
